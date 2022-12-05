6fd249008a60673511a20e30e8fff4f3c7b9987a
Weslley Ribeiro <weslley.soaresr@outlook.com>
2022-12-05 20:45:39 -0300

    Formulário de novos momentos

---
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-moment',
+  templateUrl: './new-moment.component.html',
+  styleUrls: ['./new-moment.component.css']
+})
+export class NewMomentComponent implements OnInit{
+  btnText = 'Compartilhar';
+
+  constructor(){ }
+
+  ngOnInit(): void {
+    
+  }
+}
